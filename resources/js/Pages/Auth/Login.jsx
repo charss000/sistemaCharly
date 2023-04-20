a0f063760477875f5250d22ad4e6d9735b3dae7b
charss000 <charlygabriel@reactsdigital.com>
2023-04-20 15:14:05 -0500

    ver la contraseña

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,12 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import Checkbox from '@/Components/Checkbox';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { RiMailFill,RiLock2Fill } from "react-icons/ri";
-import { FaBeer } from 'react-icons/fa';
+import { RiMailFill,RiLock2Fill,RiEyeFill,RiEyeOffFill } from "react-icons/ri";
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Login({ status, canResetPassword }) {
@@ -27,7 +25,9 @@ export default function Login({ status, canResetPassword }) {
 
         post(route('login'));
     };
-   const classIcon='absolute top-1/2 -translate-y-1/2 left-2  text-white'
+    const [showPassword, setShowPassword]=useState(false)
+   const classIcon='absolute top-1/2 -translate-y-1/2   text-white'
+   
     return (
         <GuestLayout >
             <Head title="Log in" />
@@ -43,7 +43,7 @@ export default function Login({ status, canResetPassword }) {
                 </button>
                 <div  className='relative'>
                     {/* <InputLabel htmlFor="email" value="Email"   /> */}
-                  {/* <MdMail  /> */}
+                    <RiMailFill className={classIcon + ' left-2'} />
                     <TextInput
                         id="email"
                         type="email"
@@ -55,24 +55,29 @@ export default function Login({ status, canResetPassword }) {
                         isFocused={true}
                         onChange={(e) => setData('email', e.target.value)}
                     />
-                    <RiMailFill className={classIcon} />
+                    
                     <InputError message={errors.email} className="mt-2" />
                 </div>
 
                 <div className="mt-4 relative">
                     {/* <InputLabel htmlFor="password" value="Password" /> */}
-                    <RiLock2Fill className={classIcon}/>
+                    <RiLock2Fill className={classIcon+ ' left-2'}/>
                     <TextInput
                         id="password"
-                        type="password"
+                        type={showPassword ? 'text':'password'}
                         name="password"
                         value={data.password}
-                        className="pl-8"
+                        className="px-8"
                         autoComplete="current-password"
                         placeholder='Password'
                         onChange={(e) => setData('password', e.target.value)}
                     />
+                    {showPassword ? (
+                    <RiEyeOffFill className={classIcon+' right-2 hover:cursor-pointer'}  onClick={()=>setShowPassword(!showPassword)}/>
 
+                    ):<RiEyeFill className={classIcon+' right-2 hover:cursor-pointer'} onClick={()=>setShowPassword(!showPassword)}/>
+                }
+                   
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
